ae4c7a2be9c5479d74dfc1ebf61f44d9d1c5030f
ivey <iveysaurrr@gmail.com>
2014-03-08 23:46:58 -0500

    Consolidated xhr code

--- static/connections.js
+++ static/connections.js
@@ -8,71 +8,60 @@ function initTemplates(){
 function hashChange(){
 	var data = location.hash.split('/')[1];
 	var div = document.getElementById('content');
-	var xhr = new XMLHttpRequest();
 	
-	xhr.onreadystatechange = function(){
-		if(xhr.readyState == 4){
-			var rows = JSON.parse(xhr.responseText);
-			div.innerHTML = templates[data](rows);
-		}
-	}
-
-	xhr.open("GET", '/api/' + data);
-	xhr.send(null);
+	ajax('/api/' + data, function(xhr){
+		var rows = JSON.parse(xhr);
+		div.innerHTML = templates[data](rows);
+	});
 }
 
 function deletePost(id){
-	var xhr = new XMLHttpRequest();
-	
 	if(confirm("Are you sure you want to delete this post?")){
-		xhr.onreadystatechange = function(){
-			if(xhr.readyState == 4){
-				document.getElementById('post_' + id).innerHTML = '';
-			}
-		}
-
-		xhr.open("GET", '/api/delete?id=' + id);
-		xhr.send(null);
+		ajax('/api/delete?id=' + id, function(){
+			document.getElementById('post_' + id).innerHTML = '';
+		});
 	}
 }
 
 function banUser(id, user){
-	var xhr = new XMLHttpRequest();
 	var ban = document.getElementById('ban_' + id).innerHTML;
 
 	if(confirm((ban == 'x') ? "Are you sure you want to ban " + user + "?" : "Are you sure you want to unban " + user + "?")){
-		xhr.onreadystatechange = function(){
-			if(xhr.readyState == 4){
-				if(ban == 'x')
-					document.getElementById('ban_' + id).innerHTML = 'un';
-				else
-					document.getElementById('ban_' + id).innerHTML = 'x';
-			}
-		}
+		ajax((ban == 'x') ? '/api/ban?id=' + id : '/api/unban?id=' + id, function(){
+			if(ban == 'x')
+				document.getElementById('ban_' + id).innerHTML = 'un';
+			else
+				document.getElementById('ban_' + id).innerHTML = 'x';
+		});
 	}
-
-	xhr.open("GET", (ban == 'x') ? '/api/ban?id=' + id : '/api/unban?id=' + id);
-	xhr.send(null);
 }
 
 function muteUser(id, user){
-	var xhr = new XMLHttpRequest();
 	var mute = document.getElementById('mute_' + id).innerHTML;
 
 	if(confirm((mute == 'x') ? "Are you sure you want to mute " + user + "?" : "Are you sure you want to unmute " + user + "?")){
-		xhr.onreadystatechange = function(){
-			if(xhr.readyState == 4){
-				if(mute == 'x')
+		ajax((mute == 'x') ? '/api/mute?id=' + id : '/api/unmute?id=' + id, function(){
+			if(mute == 'x')
 					document.getElementById('mute_' + id).innerHTML = 'un';
 				else
 					document.getElementById('mute_' + id).innerHTML = 'x';
-			}
+		});
+	}
+}
+
+function ajax(page, callback){
+	var xhr = new XMLHttpRequest();
+
+	xhr.onreadystatechange = function(){
+		if(xhr.readyState == 4){
+			callback(xhr.responseText);
 		}
 	}
 
-	xhr.open("GET", (mute == 'x') ? '/api/mute?id=' + id : '/api/unmute?id=' + id);
+	xhr.open("GET", page);
 	xhr.send(null);
 }
 
 window.addEventListener('load', hashChange);
 window.addEventListener('hashchange', hashChange);
+
