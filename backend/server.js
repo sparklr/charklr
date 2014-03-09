89f9c88c29f422bf6500bd741cd4f1560cb60223
ivey <iveysaurrr@gmail.com>
2014-03-08 22:20:58 -0500

    Authentication

--- backend/server.js
+++ backend/server.js
@@ -17,93 +17,72 @@ database.init();
 http.createServer(function (req, res) {
 	var requesturi = url.parse(req.url, true);
 	res.writeHead(200, {'Content-Type': 'text/html'});
-
-	if(requesturi.pathname == '/api/users'){
-		database.query("SELECT id, username, displayname, email, rank, mutetime FROM users", function(err, rows){
-			res.write(JSON.stringify(rows));
-			res.end();
-		});
-		return;
-	}
-	else if(requesturi.pathname == '/api/posts'){
-		database.query("SELECT id, `from`, message FROM timeline ORDER BY id DESC LIMIT 100", function(err, rows){
-			res.write(JSON.stringify(rows));
-			res.end();
-		});
-		return;	
-	}
-	else if(requesturi.pathname == '/api/delete'){
-		database.query("DELETE FROM timeline WHERE id = '" + parseInt(requesturi.query.id) + "'", function(err, rows){
-			res.end(200);
-		});
-	}
-	else if(requesturi.pathname == '/api/ban'){
-		database.query("UPDATE users SET rank = '-1' WHERE id = '" + parseInt(requesturi.query.id) + "'", function(err, rows){
-			res.end(200);
-		});
-	}
-	else if(requesturi.pathname == '/api/unban'){
-		database.query("UPDATE users SET rank = '0' WHERE id = '" + parseInt(requesturi.query.id) + "'", function(err, rows){
-			res.end(200);
-		});
-	}
-	else if(requesturi.pathname == '/api/mute'){
-		console.log((new Date).getTime());
-		database.query("UPDATE users SET mutetime = '" + (new Date).getTime() / 1000 + "' WHERE id = '" + parseInt(requesturi.query.id) + "'", function(err, rows){
-			res.end(200);
-		});
-	}
-	else if(requesturi.pathname == '/api/unmute'){
-		database.query("UPDATE users SET mutetime = '0' WHERE id = '" + parseInt(requesturi.query.id) + "'", function(err, rows){
-			res.end(200);
-		});
-	}
-
-	console.log("requesting: " + requesturi.pathname);
-
-	res.write(index);
-	res.end();
-}).listen(1337, '127.0.0.1');
-
-function handleRequests(request, response){
-	var requesturi = url.parse(request.url, true);
 	var sessionid;
 
-	console.log("requesturi : " + requesturi.pathname);
-
-	if(request.headers['cookie']){
-		var d = request.headers['cookie'].match(/D\=([^\s|^\;]+)\;?/);
-		sessionid = d ? d[1] : '';
+	if (req.headers["cookie"]) {
+		var d = req.headers["cookie"].match(/D\=([^\s|^\;]+)\;?/);
+		sessionid = d ? d[1] : "";
 	}
 
-	if(!request.headers['x-scheme'] && requesturi.pathname.indexOf('/rb') !== -1){
-		var s = requesturi.pathname.split('/');
-		console.log("egdfsg");
-		process.send('R:' + s[2]);
-		response.end();
-	}
+	var s = sessionid.split(",");
 
-	/*
-	if(requesturi.pathname.indexOf('/api') !== -1 || requesturi.pathname.indexOf('/beacon') !== -1){
-		Api.run(request, response, requesturi, sessionid);
-	}
-	else{
-		if(sessionid != null && sessionid != ''){
-			var s = sessionid.split(',');
+	database.query("SELECT * from users WHERE id='" + parseInt(s[0]) + "'", function(err, rows){
+		if(rows[0].authkey != s[1] || rows[0].rank < 50){
+			res.write("NOPE");
+			res.end();
+			return;
+		}
 
-			User.verifyAuth(s[0], s[1], function(success, userobj){
-				if(success)
-					Frontend.run(userobj, request, response, sessionid);
-				else
-					Frontend.showExternalPage(request, response);
+		if(requesturi.pathname == '/api/users'){
+			database.query("SELECT id, username, displayname, email, rank, mutetime FROM users", function(err, rows){
+				res.write(JSON.stringify(rows));
+				res.end();
 			});
+			return;
 		}
-		else{
-			Frontend.showExternalPage(request, response);
+		else if(requesturi.pathname == '/api/posts'){
+			database.query("SELECT id, `from`, message FROM timeline ORDER BY id DESC LIMIT 100", function(err, rows){
+				res.write(JSON.stringify(rows));
+				res.end();
+			});
+			return;	
 		}
-	}
-	*/
-}
+		else if(requesturi.pathname == '/api/delete'){
+			database.query("DELETE FROM timeline WHERE id = '" + parseInt(requesturi.query.id) + "'", function(err, rows){
+				res.end(200);
+			});
+			return;
+		}
+		else if(requesturi.pathname == '/api/ban'){
+			database.query("UPDATE users SET rank = '-1' WHERE id = '" + parseInt(requesturi.query.id) + "'", function(err, rows){
+				res.end(200);
+			});
+			return;
+		}
+		else if(requesturi.pathname == '/api/unban'){
+			database.query("UPDATE users SET rank = '0' WHERE id = '" + parseInt(requesturi.query.id) + "'", function(err, rows){
+				res.end(200);
+			});
+			return;
+		}
+		else if(requesturi.pathname == '/api/mute'){
+			console.log((new Date).getTime());
+			database.query("UPDATE users SET mutetime = '" + (new Date).getTime() / 1000 + "' WHERE id = '" + parseInt(requesturi.query.id) + "'", function(err, rows){
+				res.end(200);
+			});
+			return;
+		}
+		else if(requesturi.pathname == '/api/unmute'){
+			database.query("UPDATE users SET mutetime = '0' WHERE id = '" + parseInt(requesturi.query.id) + "'", function(err, rows){
+				res.end(200);
+			});
+			return;
+		}
+
+		res.write(index);
+		res.end();
+	});
+}).listen(1337, '127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:1337/');
 
